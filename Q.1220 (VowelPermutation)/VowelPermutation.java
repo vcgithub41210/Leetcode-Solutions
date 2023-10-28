a581cff82477656c79299b273b4a918481d1a21d
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2023-10-28 17:01:44 +0000

    now works for large numbers using modulus operator

--- Q.1220 (VowelPermutation)/VowelPermutation.java
+++ Q.1220 (VowelPermutation)/VowelPermutation.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.HashMap;
 
 class VowelPermutation{
+    private static final int MOD = 1000000007;
+    
     public static int Permutation(int vowel,int n,int[][] Vowelint,HashMap <List<Integer>,Integer> memo){
         if(n == 1) return 1;
         if(n == 2){
@@ -16,7 +18,7 @@ class VowelPermutation{
         int value = 0;
         for(int j = 0; j < Vowelint[vowel].length;j++)
         {
-            value += Permutation(Vowelint[vowel][j],n-1,Vowelint,memo);
+            value = (value + Permutation(Vowelint[vowel][j], n - 1, Vowelint, memo)) % MOD;
         }
         memo.put(List.of(n,vowel),value);
         return value;
@@ -26,13 +28,14 @@ class VowelPermutation{
         int[][] Vowelint = {{1},{0,2},{0,1,3,4},{2,4},{0}};
         HashMap<List<Integer>,Integer> memo = new HashMap<>();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the shit nigga: ");
+        System.out.println("Enter the length of string: ");
         int N = sc.nextInt();
         int permutation = 0;
         for(int i = 0; i < 5;i++)
         {
-            permutation += Permutation(i,N,Vowelint,memo);
+            permutation = (permutation + Permutation(i, N, Vowelint, memo)) % MOD;
         }
-        System.out.println(permutation);
+        System.out.println("combinations possible: " 
+        + permutation);
     }
 }
