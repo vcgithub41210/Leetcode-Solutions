3f5fe659691b260fe3792fdc03f7c7ab2c488976
vcgithub41210 <113379660+vcgithub41210@users.noreply.github.com>
2024-08-13 23:19:01 +0530

    Create SolutionJava.java

---
+++ 703. Kth Largest Element in Stream/SolutionJava.java
@@ -0,0 +1,23 @@
+class KthLargest {
+    int num_k;
+    List <Integer> array = new ArrayList<>();
+
+    public KthLargest(int k, int[] nums) {
+        Arrays.sort(nums);
+        try{
+
+            for (int i = nums.length-1; i >= nums.length-k;i--){
+                this.array.add(nums[i]);
+            }
+        }
+        catch(Exception e){
+        }
+        this.num_k = k;  
+    }
+    
+    public int add(int val) {
+        this.array.add(val); 
+        this.array.sort((a,b)->{return -1*a.compareTo(b);});
+        return this.array.get(num_k-1);
+    }
+}
